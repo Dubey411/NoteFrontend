55fe0ea5839b1c4260eb02de29135edaf36dd91e
Shubham <dubeytech9619@gmail.com>
2025-08-26 22:52:51 +0530

    Update NoteApp.jsx

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -311,7 +311,7 @@ const App = () => {
 
   // Redirect to login after popup
   setTimeout(() => {
-    window.location.href = "https://notebackend-4zqx.onrender.com/"; // or use navigate("/login") if using React Router
+    window.location.href = "https://note-frontend-psi.vercel.app/"; // or use navigate("/login") if using React Router
   }, 1600);
 };
 
